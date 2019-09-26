f569e6c2c04cd61361028bb09f4d14a2314c02f5
Flor Antara <florenciaantara@gmail.com>
2019-09-25 17:00:21 -0700

    Feature: includePaths and excludePaths

--- README.md
+++ README.md
@@ -43,3 +43,74 @@ module.exports = {
 ```
 
 For a list of all available font family options, consult the [google-fonts-plugin readme](https://github.com/SirPole/google-fonts-plugin).
+
+
+### Exclude from path
+All fonts load globally (on all pages) by default.
+
+To exclude a font from a path, use the `excludePath` property: 
+
+```javascript
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`latin`],
+            excludePath: [`/example`]
+          },
+        ],
+      },
+    }
+  ]
+}
+```
+
+### Include at path
+To add a font to a specific path, use the `includePath` property: 
+
+```javascript
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`latin`],
+            includePath: [`/example`]
+          },
+        ],
+      },
+    }
+  ]
+}
+```
+  This will load **this and all other global fonts** to this path.
+
+
+To exclude global fonts in this path, use the `excludeAllOtherFonts` property:
+
+```javascript
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`latin`],
+            includePath: [`/example`],
+            excludeAllOtherFonts: true
+          },
+        ],
+      },
+    }
+  ]
+}
+```
--- plugins/export/gatsby-ssr.js
+++ plugins/export/gatsby-ssr.js
@@ -3,13 +3,42 @@ const globby = require(`globby`).sync
 const { readFileSync } = require(`fs`)
 const path = require(`path`)
 
-exports.onRenderBody = ({pathPrefix = ``, setHeadComponents}) => {
+function checkFontProperty(file, fonts, property){
 
-	const files = globby(`./public/google-fonts/**/*.woff2`)
+	// Get all the font objects that have this property.
+	// We are looking for "includePaths" or "excludePaths".
+	const fontObjs = fonts.filter(font => font[property])
+
+	// Get the matching font for the file trying to be added
+	const matchedFont =  fontObjs && fontObjs.find(font => file.includes(font.family.toLowerCase().replace(/\s/g,``)))
+
+	if (matchedFont){
+		return matchedFont[property]
+	}
+	return false
+}
+
+function getPathsWithExcludeAllOtherFonts(fonts){
+	// Get an array of fonts that have includePaths and excludeAllOtherFonts
+	return fonts.flatMap(font => { 
+		if (!font.includePaths){
+			return
+		}
+		if (font.excludeAllOtherFonts) {
+			return font.includePaths
+		}
+	
+	}).filter(font => typeof font !== `undefined`)
+}
+
+
+exports.onRenderBody = ({ pathPrefix = ``, setHeadComponents, pathname }, {fonts = []}) => {
+	const files = globby(`./public/google-fonts/**/*.woff2`)	
 	const css = readFileSync(path.join(`./.cache/google-fonts/`, pathPrefix, `google-fonts.css`))
 	setHeadComponents([
-		...files.map((file, key) => (
-			<link
+		...files.map((file, key) => {
+
+			const linkTag = <link
 				key={`googleFont${key}`}
 				rel='preload'
 				as='font'
@@ -17,7 +46,32 @@ exports.onRenderBody = ({pathPrefix = ``, setHeadComponents}) => {
 				crossOrigin='anonymous'
 				href={file.replace(`./public`, pathPrefix)}
 			/>
-		)),
+			
+			// Make sure this font is not exluded on this path
+			if (checkFontProperty(file, fonts, `excludePaths`) 
+				&& checkFontProperty(file, fonts, `excludePaths`).includes(pathname)
+			){
+				return
+			}
+		
+			// Check whether the font should be included on a specific path only
+			// and that path is this one
+			if (checkFontProperty(file, fonts, `includePaths`)){
+				if (checkFontProperty(file, fonts, `includePaths`).includes(pathname)){
+					return linkTag
+				}  else {
+					return
+				}
+			}
+
+			// Add it globally
+			// if the current font doesn't have an includePaths or excludePaths rule
+			// and if the current path does have an include rule but it's not unique to that path
+			if (!getPathsWithExcludeAllOtherFonts(fonts).includes(pathname) ){
+				return linkTag
+			}
+
+		}),
 		<style
 			key='googleFontsCSS'
 			type='text/css'
